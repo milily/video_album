c3c5bcf0f4b102ae7303a2834d26b56ef239f7e3
mila rivas <mrise.translations@gmail.com>
2023-03-02 21:51:39 -0300

    solving first issues with git

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import AddVideo from './views/AddVideo.vue';
+</script>
+
+<template>
+    <header>
+        <div>
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div>
+    </header>
+    <div>
+        <h1>hola</h1>
+        <AddVideo />
+    </div>
+    <RouterView />
+</template>
+
+<style scoped>
+
+
+
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+
+
+@media (min-width: 1024px) {
+  
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/AddVideo.vue
@@ -0,0 +1,54 @@
+<template>
+    <a-form
+    :model="formState"
+    name="inputSearch"
+    autocomplete="off"
+    @onSearch="onSearch"
+  >
+
+      
+      
+      <a-input-search
+        v-model:value="formState.inputSearch"
+        placeholder="input search text"
+        enter-button="Search"
+        size="large"
+        @search="onSearch"
+      />
+      <a-input-search
+        v-model:value="value"
+        placeholder="input search text"
+        size="large"
+        @search="onSearch"
+      />
+       
+    </a-form>
+  </template>
+  <script lang="ts">
+    import { defineComponent, ref, reactive } from 'vue';
+    import { Form } from "ant-design-vue";
+    import "ant-design-vue/dist/antd.css";
+    interface FormState {
+  inputSearch: string;
+}
+  export default defineComponent({
+    setup() {
+        const value = ref<string>('');
+        const formState = reactive<FormState>({
+      inputSearch: 'hola'
+    });
+  
+      const onSearch = (searchValue: string) => {
+        console.log('use value', searchValue);
+        console.log('or use this.value', formState.inputSearch);
+      };
+  
+      return {
+        value,
+        formState,
+        onSearch,
+      };
+    },
+  });
+  </script>
+  
